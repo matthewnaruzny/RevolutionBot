376cf34517dc0174beb33d7affb82c371604c372
Matthew Naruzny <matthewnaruzny@gmail.com>
2021-04-05 14:00:00 -0600

    Add User Status - Admin Command

--- src/main/java/com/mnaruzny/revolutionbot/listener/commands/AdminCommandListener.java
+++ src/main/java/com/mnaruzny/revolutionbot/listener/commands/AdminCommandListener.java
@@ -1,6 +1,7 @@
 package com.mnaruzny.revolutionbot.listener.commands;
 
 import com.mnaruzny.revolutionbot.registry.DataConnector;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -40,6 +41,24 @@ public class AdminCommandListener extends ListenerAdapter {
             }
 
             if(!isAdmin && !message.getMember().isOwner() && !message.getMember().hasPermission(Permission.ADMINISTRATOR)) return;
+
+            if(command.equals("status")){
+                for(Member member : message.getMentionedMembers()){
+                    try {
+                        EmbedBuilder eb = new EmbedBuilder();
+                        eb.setTitle("User Status");
+                        eb.addField("User:", member.getNickname(), false);
+                        eb.addField("ID:", member.getId(), false);
+
+                        boolean isMAdmin = dataConnector.getMemberSettings(member.getIdLong(), member.getGuild().getIdLong()).isAdmin();
+                        eb.addField("Is Admin:", (isMAdmin ? "True" : "False"), true);
+
+                        message.getTextChannel().sendMessage(eb.build()).queue();
+                    } catch (SQLException exception) {
+                        message.getTextChannel().sendMessage("Error").queue();
+                    }
+                }
+            }
 
             // Clear chat history
             if(command.equals("kick")){
