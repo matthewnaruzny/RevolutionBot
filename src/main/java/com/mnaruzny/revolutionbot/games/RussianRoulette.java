de28eddcb15ccaeec76f2452768cd5064f386489
Matthew Naruzny <matthewnaruzny@gmail.com>
2021-04-20 13:40:03 -0600

    Add Class for Russian Roulette mini game

---
+++ src/main/java/com/mnaruzny/revolutionbot/games/RussianRoulette.java
@@ -0,0 +1,13 @@
+package com.mnaruzny.revolutionbot.games;
+
+import net.dv8tion.jda.api.entities.Guild;
+
+public class RussianRoulette {
+
+    private Guild guild;
+
+    public RussianRoulette(Guild guild){
+        this.guild = guild;
+    }
+
+}
